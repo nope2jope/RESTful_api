6bf6b25c71cb2d6b9a1d32d21ab41fe895bc418d
nope2jope <67401076+nope2jope@users.noreply.github.com>
2021-05-24 18:14:38 -0700

    improved api functionality
    added POST, PATCH, DELETE functionality, personal notes

--- main.py
+++ main.py
@@ -12,7 +12,6 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 
-
 ##Cafe TABLE Configuration
 class Cafe(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -26,6 +25,7 @@ class Cafe(db.Model):
     has_sockets = db.Column(db.Boolean, nullable=False)
     can_take_calls = db.Column(db.Boolean, nullable=False)
     coffee_price = db.Column(db.String(250), nullable=True)
+
 
 def j(x):
     return jsonify({
@@ -45,10 +45,10 @@ def j(x):
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template('index.html')
 
 
-# http GET route
+# display random cafe
 @app.route('/random')
 def choose_random():
     cafes = db.session.query(Cafe).all()
@@ -56,7 +56,8 @@ def choose_random():
     return j(x=choice)
 
 
-@app.route("/all")
+# display all cafes — takes some type conversion due to trouble with jsonify
+@app.route('/all', methods=['GET'])
 def get_cafes():
     cafes = db.session.query(Cafe).all()
 
@@ -66,29 +67,87 @@ def get_cafes():
         entry = j(x=c).json
         cafe_list.append(entry)
 
-    # convert list to dictionary — why?
+    # convert list to dictionary for json output on screen
     cafe_json = {}
     for item in cafe_list:
         name = item['name']
         cafe_json[name] = item
 
-    # example of iteration (personal reference)
-    # for x in cafe_json:
-    #     print(cafe_json[x]['has_wifi'])
-
     return cafe_json
 
 
-
+# search via api for specific location, tenders first result
 @app.route('/search')
 def search():
-
-    query_location = request.args.get("loc")
+    query_location = request.args.get('loc')
     cafe = db.session.query(Cafe).filter_by(location=query_location).first()
     if cafe:
         return j(x=cafe)
     else:
-        return jsonify(error={"Not Found": "Sorry, we don't have a cafe at that location."})
+        return jsonify(error={'Not Found': "Sorry, we don't have a cafe at that location."})
+
+
+# adds cafe to database
+@app.route('/add', methods=['POST'])
+def add():
+    new_cafe = Cafe(
+        name=request.form.get('name'),
+        map_url=request.form.get('map_url'),
+        img_url=request.form.get('img_url'),
+        location=request.form.get('location'),
+        seats=request.form.get('seats'),
+
+        # booleans would TypeError crash without parser json.loads()
+        has_toilet=json.loads(request.form.get('has_toilet')),
+        has_wifi=json.loads(request.form.get('has_wifi')),
+        has_sockets=json.loads(request.form.get('has_sockets')),
+        can_take_calls=json.loads(request.form.get('can_take_calls')),
+
+        coffee_price=request.form.get('coffee_price'),
+    )
+
+    db.session.add(new_cafe)
+    db.session.commit()
+
+    return redirect(url_for('home'))
+
+
+# updates coffee price of a given cafe via id (id can be retreived via a get request)
+@app.route('/update_price/<string:cafe_id>', methods=['GET', 'PATCH'])
+def update_price(cafe_id):
+    query_cafe = Cafe.query.get(cafe_id)
+
+    if query_cafe:
+        new_price = request.form.get('new_price')
+        query_cafe.coffee_price = new_price
+        db.session.commit()
+
+        # passes along updated json as confirmation, plus 200 status code
+        return j(x=query_cafe), 200
+
+        # passes along json formatted error message (in keeping with style), plus 404 re: id
+    else:
+        return {'error': {'not found': 'no such id found'}}, 404
+
+
+@app.route('/close_shop/<string:cafe_id>', methods=['GET', 'DELETE'])
+def remove_entry(cafe_id):
+
+    target_cafe = Cafe.query.get(cafe_id)
+
+    if target_cafe:
+        secret_key = request.form.get('secret_key')
+        if secret_key == 'TopSecretKey':
+            db.session.delete(target_cafe)
+            db.session.commit()
+            return {'removed': {'success': f'{target_cafe.name} deleted'}}, 404
+
+        else:
+            return {'error': {'forbidden': 'authorization failed'}}, 403
+
+    else:
+        return {'error': {'not found': 'no such id found'}}, 404
+
 
 if __name__ == '__main__':
     app.run(debug=True)
